6035231cecbe08dc4a6d4c3050640f5c5f7f8bed
Diego Agreda <diegoagredahidalgo@gmail.com>
2023-05-27 10:53:28 -0500

    Control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { Product, onChangeArgs } from "../interfaces/interfaces"
 
 interface useProductArgs {
@@ -9,13 +9,8 @@ interface useProductArgs {
 
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
     const [counter, setCounter] = useState(value);
-    const isControlled = useRef(!!onChange);
     
     const increaseBy = (value: number) => {
-        if (isControlled.current){
-            return onChange!({count:value, product}); //! este signo en este caso no es negacion, es para decirle a typescript que omita la comprobacion o el mensaje de warning 
-        }
-
         const newValue = Math.max(counter + value, 0)
         setCounter(newValue)
         onChange && onChange({ count: newValue, product });
